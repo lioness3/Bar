16d7836a0a7bfe791fb4203e487b70bce49bee19
lioness3 <jcnailtech3@yahoo.com>
2020-02-14 14:11:20 -0800

    make body background black and add a grid to new inventory form

--- src/components/NewInventoryForm.js
+++ src/components/NewInventoryForm.js
@@ -2,11 +2,32 @@ import React from 'react';
 
 
 function NewInventoryForm() {
+  const background = {
+
+  }
+  const inventorFormStyle = {
+    display:'grid',
+    gridTemplateColumns:' auto auto auto',
+    gridGap:'5px' ,
+     border:'1px solid black',
+    position:'absolute',
+    top:'200px',
+    width:'600px',
+    backgroundColor:'grey',
+
+  }
+  const inventoryBtn = {
+    color:'Red'
+  }
   return (
-    <div>
-      <form>
-    <input type='' id= '' placeholder= ''/>
-<button type='submit'>SUBMIT</button>
+    <div style={background}>
+      <form style={inventorFormStyle}>
+        <input type='text' id= '' placeholder= 'Brand Name'/>
+        <input type='integer' id= '' placeholder='Alcohol Content'/>
+        <input type='text' id= '' placeholder= 'Type of Beer'/>
+        <input type='text' id= '' placeholder= 'Name of Beer'/>
+        <input type='integer' id= '' placeholder='Price per Keg'/>
+        <button style={inventoryBtn} type='submit'>SUBMIT</button>
       </form>
     </div>
   );
