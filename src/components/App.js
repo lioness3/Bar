aae334b2d07a13cc381625250419d6cc737b72e6
lioness3 <jcnailtech3@yahoo.com>
2020-02-14 10:10:02 -0800

    create navigation bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation.js';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-
+      <Navigation/>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Navigation() {
+  const Nav_bar = {
+    position:'absolute',
+    height:'100px',
+    backgroundColor:'black',
+    width:'100vw'
+  }
+  return (
+    <div style={Nav_bar}>
+
+    </div>
+  );
+}
+
+export default Navigation;
