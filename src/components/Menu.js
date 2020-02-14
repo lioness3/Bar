f5f3c9be76876fd5d49f04093db81e03bd3db80f
lioness3 <jcnailtech3@yahoo.com>
2020-02-14 13:14:02 -0800

    add menu drop down

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,45 @@ function Menu() {
   return (
     <div className='menuDropDown'>
       <button className='menuDropDownBtn'>MENU</button>
+      <div className='menuContent'>
+        <span>
+          <ul>
+            <h3>IPA</h3>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </span>
+        <span>
+          <ul>
+            <h3>Lager</h3>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </span>
+        <span>
+          <ul>
+            <h3>Stout</h3>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </span>
+        <span>
+          <ul>
+            <h3>Pilsner</h3>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </span>
+
+      </div>
 
     </div>
   );
