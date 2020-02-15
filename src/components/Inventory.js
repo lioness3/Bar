2ff2c3ac39359b87b7841b804a7025390abef90a
lioness3 <jcnailtech3@yahoo.com>
2020-02-15 15:52:41 -0800

    add filter

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -12,7 +12,6 @@ function Inventory() {
     overflowX:'scroll',
     padding:'40px',
 
-
   }
 
   return (
--- src/components/Keg.js
+++ src/components/Keg.js
@@ -5,17 +5,23 @@ import taps from './images/taps.png';
 
 function Keg() {
 const kegStyle = {
+textAlign:'center',
+margin:'5px',
+background: 'rgba(0,0,0,0.3)',
 
+color:'rgba(150,150,150,0.6)',
+fontSize: '20px',
+textDecoration: 'none',
 
 }
   return (
-    <div className='kegStyle'>
-      <KegDropDown/>
+    <div style={kegStyle}>
       <h1>BRAND</h1>
       <h2>Name of Beer</h2>
       <h3>Alcohol Content</h3>
       <h5>Type of Beer</h5>
-      <img alt="keg" src={taps}></img>
+      <img alt="keg" src={taps} style={{filter: 'drop-shadow(0px 75px 20px  black) sepia(80%) grayscale(10%) opacity(90%)'}}></img>
+      <KegDropDown/>
         <Count count={10}/>
     </div>
   );
