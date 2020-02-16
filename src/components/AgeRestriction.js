778dca7880529f0abd2b13dc18c393559531dd79
lioness3 <jcnailtech3@yahoo.com>
2020-02-15 19:11:51 -0800

    add age restriction styling

--- src/components/AgeRestriction.js
+++ src/components/AgeRestriction.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './AgeRestriction.css';
+import { Link } from 'react-router-dom';
 
 
 function AgeRestriction() {
 
 
   return (
-<div></div>
+  <div style={{margin:'500px', textAlign:'center'}}>
+    <div className='ageRestrictionStyle'>
+      <h1>ARE YOU 21+?</h1>
+      <div className='yesOrNoLinks'>
+        <Link to='/inventory'>😁</Link>
+        <Link to='/'>😩</Link>
+      </div>
+    </div>
+  </div>
   );
 }
 
